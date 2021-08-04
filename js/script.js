bbad11361fa67874a9c5b46dc85f2c50e126905e
amy_p <ampreci@gmail.com>
2021-08-04 13:50:10 -0700

    displayed repos

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const overview = document.querySelector(".overview");//Div with my profile info
 const username = "Amy-Pr";
+const reposList = document.querySelector(".repo-list"); //the unordered list to display my repos
+
+
 const getData = async function() {
     const res = await fetch (`https://api.github.com/users/${username}`);
     const data = await res.json();
@@ -11,7 +14,7 @@ getData();
 
 const showProfile = function (data) {
     const div = document.createElement("div");
-    div.classList.add("username");
+    div.classList.add("user-info");
     div.innerHTML = 
     `<figure>
     <img alt="user avatar" src=${data.avatar_url}/>
@@ -23,4 +26,22 @@ const showProfile = function (data) {
     <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
   </div>`;
   overview.append(div);
-}
+};
+
+const getRepos = async function() {
+    const res = await fetch (`https://api.github.com/users/${username}/repos?sort=pushed&per_page=100`);
+    const data = await res.json();
+    console.log(data);
+    showRepos(data);
+};
+getRepos ();
+
+const showRepos = function (repos) {
+    for (repo of repos) {
+        const li = document.createElement("li");
+        li.classList.add("repo");
+        li.innerHTML = `<h3>${repo.name}</h3>`;
+        reposList.append(li);
+    };
+
+};
